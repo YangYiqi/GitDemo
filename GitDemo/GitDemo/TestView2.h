7056f82f43c12e6cb956480fa2f3f8fb8efa7f90
yyq <398264361@qq.com>
2015-11-11 15:49:36 +0800

    add2

---
+++ GitDemo/GitDemo/TestView2.h
@@ -0,0 +1,13 @@
+//
+//  TestView2.h
+//  GitDemo
+//
+//  Created by qianfeng on 15/11/11.
+//  Copyright (c) 2015年 Yiqi.Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView2 : UIView
+
+@end
---
+++ GitDemo/GitDemo/TestView2.m
@@ -0,0 +1,21 @@
+//
+//  TestView2.m
+//  GitDemo
+//
+//  Created by qianfeng on 15/11/11.
+//  Copyright (c) 2015年 Yiqi.Yang. All rights reserved.
+//
+
+#import "TestView2.h"
+
+@implementation TestView2
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
